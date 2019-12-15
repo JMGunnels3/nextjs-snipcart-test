5ff99d9fc4eb0826a6f398690fe34b5c79e7988f
justin <jmgunnels3@gmail.com>
2019-12-15 16:15:49 -0600

    Trying to debug crawling issue with data-item-url

--- components/ProductDefinition.js
+++ components/ProductDefinition.js
@@ -5,7 +5,7 @@ const ProductDefinition = (props) => {
       data-item-name={props.product.name}
       data-item-price={props.product.price}
       data-item-image={props.product.image}
-      data-item-url="/"
+      data-item-url="https://unruffled-allen-9288e9.netlify.com/"
     >
       Add to cart ${props.product.price}
     </button>
